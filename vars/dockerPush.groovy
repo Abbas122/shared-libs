f3db6934707b2348e77ba21fe28cd3e957acccfa
Abbas122 <52916647+Abbas122@users.noreply.github.com>
2025-06-28 13:34:36 +0500

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,1 +1,11 @@
-
+def call(String dockerCredId, String imageName) {
+    withCredentials([usernamePassword(
+           credentialsId: dockerCredId, 
+            usernameVariable: 'DOCKER_USER', 
+            passwordVariable: 'DOCKER_PASS')]) {
+            
+                sh "docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}" 
+                sh "docker image tag ${imageName}:latest ${DOCKER_USER}/${imageName}:latest"
+                sh "docker push ${DOCKER_USER}/${imageName}:latest"
+        }
+}
